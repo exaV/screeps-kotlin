e6b93a632f62de9b69f8d194eb18ac88d75537f6
Patrick Del Conte <p.del.conte@hotmail.ch>
2018-05-20 14:48:41 +0200

    add serializable KreepSpawnOptions

--- src/main/kotlin/screeps/game/one/Spawning.kt
+++ src/main/kotlin/screeps/game/one/Spawning.kt
@@ -3,8 +3,8 @@ package screeps.game.one
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JSON
 import screeps.game.one.kreeps.BodyDefinition
-import screeps.game.one.kreeps.KreepSpawnOptions
 import types.base.global.*
+import types.base.prototypes.structures.SpawnOptions
 import types.base.prototypes.structures.StructureSpawn
 
 fun StructureSpawn.spawn(bodyDefinition: BodyDefinition, spawnOptions: KreepSpawnOptions? = null): Boolean {
@@ -13,13 +13,18 @@ fun StructureSpawn.spawn(bodyDefinition: BodyDefinition, spawnOptions: KreepSpaw
     val body = bodyDefinition.getBiggest(room.energyAvailable)
     val newName = "${bodyDefinition.name}_T${body.tier}_${Game.time}"
 
-    val code = this.spawnCreep(body.body.toTypedArray(), newName, spawnOptions ?: KreepSpawnOptions(CreepState.REFILL))
-    when (code) {
+    val actualSpawnOptions = spawnOptions ?: GlobalSpawnQueue.defaultSpawnOptions
+    val code = this.spawnCreep(
+        body.body.toTypedArray(),
+        newName,
+        actualSpawnOptions.toSpawnOptions()
+    )
+    return when (code) {
         OK -> {
             println("spawning $newName with body $body")
-            return true
+            true
         }
-        ERR_NOT_ENOUGH_ENERGY, ERR_BUSY -> return false // do nothing
+        ERR_NOT_ENOUGH_ENERGY, ERR_BUSY -> false // do nothing
         else -> throw IllegalArgumentException("error code $code when spawning $newName with body $body")
     }
 }
@@ -32,15 +37,21 @@ object GlobalSpawnQueue {
         get() = queue
 
     private var modified: Boolean = false
+    val defaultSpawnOptions = KreepSpawnOptions(state = CreepState.IDLE)
 
     init {
         // load from memory
-        queue = Memory.globalSpawnQueue?.queue?.toMutableList() ?: ArrayList()
-        println("spawnqueue initialized")
+        queue = try {
+            Memory.globalSpawnQueue?.queue?.toMutableList() ?: ArrayList()
+        } catch (e: Error) {
+            println("Error while initializing GlobalSpawnQueue: $e")
+            ArrayList()
+        }
+        println("spawnqueue initialized to $queue")
     }
 
     fun push(bodyDefinition: BodyDefinition, spawnOptions: KreepSpawnOptions? = null) {
-        queue.add(SpawnInfo(bodyDefinition))
+        queue.add(SpawnInfo(bodyDefinition, spawnOptions ?: defaultSpawnOptions))
         modified = true
     }
 
@@ -80,12 +91,42 @@ object GlobalSpawnQueue {
         }
 }
 
-fun requestCreep(bodyDefinition: BodyDefinition, spawnOptions: KreepSpawnOptions? = null) {
-    GlobalSpawnQueue.push(bodyDefinition, spawnOptions)
+fun requestCreep(bodyDefinition: BodyDefinition, spawnOptions: KreepSpawnOptions) {
+
+    val candidate = Context.creeps.values.firstOrNull { it.body.contentEquals(bodyDefinition.bodyPartConstant) }
+    if (candidate != null) {
+        spawnOptions.transfer(candidate.memory)
+    } else {
+        GlobalSpawnQueue.push(bodyDefinition, spawnOptions)
+    }
+
 }
 
 @Serializable
-data class SpawnInfo(val bodyDefinition: BodyDefinition)
+data class SpawnInfo(val bodyDefinition: BodyDefinition, val spawnOptions: KreepSpawnOptions)
 
 @Serializable
 data class CreepSpawnList(val queue: List<SpawnInfo>)
+
+@Serializable
+class KreepSpawnOptions(
+    private val state: CreepState = CreepState.IDLE,
+    private val missionId: String? = null,
+    private val targetId: String? = null,
+    private val assignedEnergySource: String? = null
+) {
+    fun toSpawnOptions(): SpawnOptions {
+        return object : SpawnOptions {
+            override val memory = object : CreepMemory {}.apply { transfer(this) }
+        }
+    }
+
+    fun transfer(memory: CreepMemory) {
+        memory.state = state
+        memory.missionId = missionId
+        memory.targetId = targetId
+        memory.assignedEnergySource = assignedEnergySource
+    }
+}
+
+
--- src/main/kotlin/screeps/game/one/UpgradeMission.kt
+++ src/main/kotlin/screeps/game/one/UpgradeMission.kt
@@ -1,24 +1,11 @@
 package screeps.game.one
 
 import screeps.game.one.kreeps.BodyDefinition
-import screeps.game.one.kreeps.KreepSpawnOptions
 import traveler.travelTo
 import types.base.global.ERR_NOT_IN_RANGE
 import types.base.global.Game
-import types.base.global.Memory
 import types.base.prototypes.Creep
 import types.base.prototypes.structures.StructureController
-
-class Missions(private val backing: MutableList<String>) : MutableList<String> by backing {
-
-}
-
-var Memory.missions: Missions
-    get() = JSON.parse(this.asDynamic().missions)
-    set(value) {
-        this.asDynamic().missions = value
-    }
-
 
 /**
  * Mission to upgrade a controller using multiple creeps to carry energy
@@ -33,14 +20,12 @@ class UpgradeMission(val controllerId: String) {
     private val workers: MutableList<Creep> = mutableListOf()
 
     init {
-
         val controllerFromMemory = Game.getObjectById<StructureController>(controllerId)
         controller = controllerFromMemory ?:
                 throw IllegalStateException("could not load controller for id $controllerId") // captured
 
         workers.addAll(Context.creeps.values.filter { it.memory.missionId == missionId })
     }
-
 
     fun execute() {
 
@@ -59,9 +44,6 @@ class UpgradeMission(val controllerId: String) {
                 worker.travelTo(controller.pos)
             }
         }
-
     }
-
-
 }
 
